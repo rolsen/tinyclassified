0f049558ba8ac20f0b774cff52ce90a9f2fa69c0
David Wong <davidwong.xc@gmail.com>
2014-08-05 11:34:25 -0700

    fix ads on resource directory pages

--- config_cache.py
+++ config_cache.py
@@ -1,7 +1,7 @@
 OVERWRITE_CONFIG = {
     'config': None,
     'db_adapter': None,
-    'get_common_template_vals': lambda x: {},
+    'get_common_template_vals': lambda x: {"top_ad_target": "Resources", "side_ad_1_target": "Resources", "side_ad_2_target": "Resources"},
     'app': None
 }
 
